cdb3115c609946d8db3d9af7ba2108a0309f2e97
Nico <nicosiput@gmail.com>
2021-06-25 15:24:19 +0700

    adding route static for assets

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('images'));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
-app.use(express.static('images'));
 const server = app.listen(app.get('port'), () => {
   console.log('listening on port ', server.address().port);
 });
